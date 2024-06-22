1cd1d1e43c9609a5c641d58ce7a40383a603adbe
HITESHDALVI <70736293+HITESHDALVI@users.noreply.github.com>
2024-06-22 16:21:17 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  Image,
   Platform,
   StatusBar,
   StyleSheet,
@@ -11,30 +10,32 @@ import {
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import DocumentPicker, {
-  DirectoryPickerResponse,
-  DocumentPickerResponse,
-} from 'react-native-document-picker';
-import RNFetchBlob from 'rn-fetch-blob';
-
+import DocumentPicker from 'react-native-document-picker';
+import RNFetchBlob from 'react-native-blob-util';
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
   const [result, setResult] = React.useState<
-    Array<DocumentPickerResponse> | DirectoryPickerResponse | undefined | null
-  >();
+    | {
+        name: string;
+        type: string;
+        size: number | null;
+        extension: string;
+        blob: any;
+        path: string;
+      }[]
+    | undefined
+    | null
+  >(null);
 
-  async function pickAndProcessFile() {
+  const handleDocumentUpload = async () => {
     try {
       const res = await DocumentPicker.pickSingle();
-
       if (!res) {
         throw new Error('File pick cancelled or failed');
       }
-
       const {
         type: fileType,
         uri: fileUri,
@@ -48,9 +49,9 @@ function App(): React.JSX.Element {
       const fileExtension = fileType.substr(fileType.indexOf('/') + 1);
       let realURI = fileUri;
       if (Platform.OS === 'ios') {
-        realURI = decodeURI(fileUri); // Decode URI for iOS
+        realURI = decodeURI(fileUri);
       }
-      const base64Data = await readBase64Data(realURI);
+      const base64Data = await getBase64Data(realURI);
       const sanitizedFileName = fileName.replace(/\s/g, '');
       const path = '/patients';
 
@@ -64,14 +65,13 @@ function App(): React.JSX.Element {
           path: Array.isArray(path) ? path.join() : path,
         },
       ];
-
-      console.log({newUploadedFile});
+      setResult(newUploadedFile);
     } catch (error) {
       console.error('Error picking or processing file:', error);
     }
-  }
+  };
 
-  async function readBase64Data(uri) {
+  const getBase64Data = async (uri: string) => {
     try {
       const base64Data = await RNFetchBlob.fs.readFile(uri, 'base64');
       return base64Data;
@@ -79,8 +79,7 @@ function App(): React.JSX.Element {
       console.error('Error reading file as base64:', error);
       throw new Error('Failed to read file');
     }
-  }
-
+  };
   return (
     <View style={styles.wrapper}>
       <StatusBar
@@ -90,7 +89,7 @@ function App(): React.JSX.Element {
       <TouchableOpacity
         style={[styles.button]}
         onPress={async () => {
-          pickAndProcessFile();
+          handleDocumentUpload();
         }}>
         <Text style={[{color: 'black', fontFamily: '800', fontSize: 18}]}>
           Upload
